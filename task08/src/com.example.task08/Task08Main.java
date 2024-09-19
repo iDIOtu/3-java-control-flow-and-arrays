f0e243b364d5afc1e6f71dfa87368a8750b606c3
nikit <nikitonnoev@gmail.com>
2024-09-19 21:41:57 +0500

    Голубев Никита. Задание 08

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,16 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+
+        long multiply = 1;
+
+        if (arr.length == 0) return 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            multiply *= arr[i];
+        }
+
+        return multiply;
     }
 
 }
