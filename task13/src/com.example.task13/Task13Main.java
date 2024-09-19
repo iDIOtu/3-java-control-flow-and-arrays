343aa46f35d82a57d04b4b30cc92865ee2c8764f
nikit <nikitonnoev@gmail.com>
2024-09-19 22:15:27 +0500

    Голубев Никита. Задание 13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -12,8 +12,30 @@ public class Task13Main {
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+
+        if (arr == null) {
+            return null;
+        }
+
+        int count = 0;
+
+        for (int num : arr) {
+            if (num <= 1000) {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int num : arr) {
+            if (num <= 1000) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
     }
+
 
 }
